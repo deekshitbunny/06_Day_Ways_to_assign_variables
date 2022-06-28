5dc751e2ea052a07a693256a79060a09cb38753f
Deekshitth Vegi <65802453+deekshitbunny@users.noreply.github.com>
2022-06-28 17:08:57 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,21 @@
+a=3
+b=21
+c=4
+print(a)
+print(b)
+print(c)
+
+print(a,b,c)
+
+print(a) ; print(b) ; print(c)
+
+p,q,r=2,3,4
+
+print(p) ; print(q) ; print(r)
+
+print(type(34))
+print(type('eruyg'))
+print(type(.34))
+print(type(0.23))
+print(type('''6'''))
+#print(type(ehr)) Nameerror
